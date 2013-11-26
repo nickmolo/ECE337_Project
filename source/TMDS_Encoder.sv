78c192bf27a48e8ac07f5f751dff8e45aa59710d
Nick Molo <nick.molo@gmail.com>
2013-11-25 20:40:01 -0500

    asd - Just for you brian

--- source/TMDS_Encoder.sv
+++ source/TMDS_Encoder.sv
@@ -1,52 +1,52 @@
 module TMDS_Encoder
 (
-	input reg [7:0] video_data,
-	input reg [7:0] blank_data,
-	input wire [1:0] blank_sel,
-	input wire D1_load,
-	input wire D2_load,
-	input wire S1_load,
-	input wire S2_load,
-	input wire SR0_load,
-	input wire SR1_load,
-	input wire out_sel,
+	input wire clk,
+	input wire sr_clk,
+	input wire n_rst,
+	input wire s_rst,		//Sync reset to stage 2 from TMDS controller
+	input reg D1_load,		//load enables from TMDS controller
+	input reg D2_load,
+	input reg S1_load,
+	input reg S2_load,
+	input reg L2_load,
+	input reg SR0_load,		//load enables from Output controller
+	input reg SR1_load,
+	input reg [7:0] pixel_data,
+	input reg [9:0] preamble_data,
+	input reg [9:0] guard_data,
+	input wire [1:0] out_sel,
+	input wire shiftmuxsel,
 	output wire TMDS_out
 );
 
 //Internal Transitional Registers
-reg [7:0] D1_input_data;	//D1 input from Blanking mux
-reg [7:0] D2_input_data;	//D2 input from D1
-reg [7:0] L1_input_data;	//L1 input from D2
-reg [8:0] S1_input_data;	//S1 input from L1
-reg [8:0] L2_input_data;	//L2 input from S1
-reg [9:0] S2_input_data;	//S2 input from L2
+reg [7:0] D2_input;	//D2 input from D1
+reg [7:0] L1_input;	//L1 input from D2
+reg [8:0] S1_input;	//S1 input from L1
+reg [8:0] L2_input;	//L2 input from S1
+reg [9:0] S2_input;	//S2 input from L2
+reg [9:0] pixel_encoded;	//Final encoded pixel data
+reg [9:0] SR_input;
 
-//Select between input video data and blanking mode data
-blankMux BMUX
-	(
-		.videoData(video_data),
-		.blankData(blank_data),
-		.guardData(guard_data),
-		.sel(blank_sel),
-		.out(D1_input)
-	);
 
 //Updated with valid video or blanking data
-buffer D1BUFF
+buffer #(8) D1BUFF
 	(
 		.clk(clk),
 		.n_rst(n_rst),
-		.p_in(D1_input),
-		.p_out(D2_input)
+		.p_in(pixel_data),
+		.p_out(D2_input),
+		.load_enable(D1_load)
 	);
 
 //Stores input to first stage of encoder
-buffer D2BUFF
+buffer #(8) D2BUFF
 	(
 		.clk(clk),
 		.n_rst(n_rst),
 		.p_in(D2_input),
-		.p_out(L1_input)
+		.p_out(L1_input),
+		.load_enable(D2_load)
 	);
 
 //Computes new 9-bit value using the 8-bits from D2
@@ -57,37 +57,51 @@ stageOne LOGIC1
 	);	
 	
 //Stores 9-bit value from stage 1
-buffer S1BUFF
+buffer #(9) S1BUFF
 	(
 		.clk(clk),
 		.n_rst(n_rst),
 		.p_in(S1_input),
-		.p_out(L2_input)
+		.p_out(L2_input),
+		.load_enable(S1_load)
 	);
 
 //10-bit from D2, 9-bit output to S1
-stageTwo LOGIC2
+stage_2 LOGIC2
 	(
 		.clk(clk),
 		.n_rst(n_rst),
+		.s_rst(s_rst),
 		.load(L2_load),
 		.data_in(L2_input),
 		.data_out(S2_input)
 	);
 
 //Stores 10-bit value from stage 2
-buffer S2BUFF
+buffer #(10) S2BUFF
 	(
 		.clk(clk),
 		.n_rst(n_rst),
 		.p_in(S2_input),
-		.p_out(SR_input)
+		.p_out(pixel_encoded),
+		.load_enable(S2_load)
 	);
 
-//SR0, shifts while SR1 is loading
-hr_sv SR0
+//Selects between various inputs
+outputMux BMUX
 	(
-                .clk(clk),
+		.videoData(pixel_encoded),
+		.blankData(preamble_data),
+		.guardData(guard_data),
+		.sel(out_sel),
+		.out(SR_input)
+	);
+
+
+//SR0, shifts while SR1 is loading
+hs_sr SR0
+	(
+                .clk(sr_clk),
                 .n_rst(n_rst),
                 .load_enable(SR0_load),
                 .parallel_in(SR_input),
@@ -95,7 +109,7 @@ hr_sv SR0
         );
 
 //SR1, shifts while SR0 is loading
-hr_sv SR1
+hs_sr SR1
 	(
                 .clk(clk),
                 .n_rst(n_rst),
@@ -107,10 +121,12 @@ hr_sv SR1
 //Choosese the SR that is currently shifting out
 sr_mux SRMUX
 	(
-                .select(out_sel),
+                .select(shiftmuxsel),
                 .sr0(SR0_out),
                 .sr1(SR1_out),
                 .muxout(TMDS_out)
         );
+
+
 
 endmodule
---
+++ source/stage_2.sv
@@ -0,0 +1,104 @@
+module stage_2
+    
+  (
+  
+    input wire clk,
+    input wire n_rst,
+    input wire load,
+    input wire s_rst,
+    input wire [8:0]data_in,
+    output reg [9:0]data_out
+
+  );
+    
+  reg [15:0]disparity_cnt;
+  reg [15:0]nxt_disparity_cnt;
+  reg [15:0]current_cnt;
+  reg [2:0]num_1;
+  reg [2:0]num_0;
+  
+  //disparity counter reg behaviour
+  always @ (posedge clk, negedge n_rst) begin: state_reg
+  
+    //if reset is low goto 0
+    if(!n_rst) begin
+      disparity_cnt <= 0;
+    end
+    
+    //otherwise goto next state
+    else begin
+      disparity_cnt <= nxt_disparity_cnt;
+    end
+  
+  end
+  
+  
+  //Combinational next state logic
+  always @ (disparity_cnt, current_cnt, load) begin: nxt_state_logic 
+    
+    if(s_rst == 1) begin
+      nxt_disparity_cnt = 0;
+    end
+
+    else if(load == 1) begin
+      nxt_disparity_cnt = current_cnt; 
+    end
+      
+    else begin
+      nxt_disparity_cnt = disparity_cnt;
+    end
+  end
+   
+  
+  //stage 2 combo logic
+  always @ (disparity_cnt, num_0, num_1, data_in) begin: stage_2_logic
+    
+    if((disparity_cnt == 0) || (num_1 == num_0)) begin
+      
+      data_out[9] = !data_in[8];
+      data_out[8] = data_in[8];
+      
+      if(data_in[8] == 0) begin
+        
+        data_out[7:0] = ~data_in[7:0];  
+        
+        current_cnt = disparity_cnt + num_0 - num_1;
+        
+      end
+      
+      else begin
+        
+        data_out[7:0] = data_in[7:0];  
+        
+        current_cnt = disparity_cnt + num_1 - num_0;
+        
+      end
+      
+    end
+      
+    else if(((disparity_cnt > 0) && (num_1 > num_0)) || ((disparity_cnt < 0) && (num_0 > num_1))) begin
+      
+      data_out[9] = 1;
+      data_out[8] = data_in[8];
+      data_out[7:0] = ~data_in[7:0];
+      
+      current_cnt = disparity_cnt + ((2'd2) * data_in[8]) + num_0 - num_1;
+      
+    end
+      
+    else begin
+      
+      data_out[9] = 0;
+      data_out[8] = data_in[8];
+      data_out[7:0] = data_in[7:0];
+      
+      current_cnt = disparity_cnt + ((2'd2) * !data_in[8]) + num_1 - num_0;
+      
+    end  
+  
+  end
+        
+  assign num_1 = data_in[0] + data_in[1] + data_in[2] + data_in[3] + data_in[4] + data_in[5] + data_in[6] + data_in[7];
+  assign num_0 = 3'd8 - num_1;      
+  
+endmodule
